80d8f7bededbfa8a14a75f740c6f12be8204b349
Lara <lrpoblet@gmail.com>
2023-03-20 12:47:49 +0100

    add react templateç

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+// import {useEffect, useState} from 'react';
+// import {Route} from 'react-router-dom';
+// import callToApi from '../services/api';
+// import ls from '../services/localStorage';
+
+import '../styles/App.scss';
+
+function App() {
+  // VARIABLES ESTADO
+
+  // USE EFFECT
+
+  // FUNCIONES HANDLER
+
+  // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR HTML
+
+  // HTML EN EL RETURN
+
+  return (
+    <div className="App">
+      <h1 className="title">Hola mundo</h1>
+
+      {/* Código HTML. */}
+    </div>
+  );
+}
+
+export default App;
